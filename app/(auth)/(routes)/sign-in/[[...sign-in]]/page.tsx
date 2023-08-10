55ef422e4374acb85192738fc3a87d54bda86a6b
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-10 15:22:07 -0300

    essential clerk configuration

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
 
-export default function Home() {
+export default function Page() {
   return <SignIn />
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import AuthLayout from './(auth)/layout'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <AuthLayout />
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
