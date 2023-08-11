fc436a32b3cd1ce26f30c24abb4b3d299197751b
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-11 17:41:29 -0300

    new ui components and setting form modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,8 @@ import { useEffect } from 'react'
 import { useStoreModal } from '@/hooks/useStoreModal'
 
 export default function SetupPage() {
-  const isOpen = useStoreModal((state) => state.isOpen)
-  const onOpen = useStoreModal((state) => state.onOpen)
+  const { isOpen, onOpen } = useStoreModal()
+  // const onOpen = useStoreModal((state) => state.onOpen)
 
   useEffect(() => {
     if (!isOpen) {
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,8 +1,37 @@
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useStoreModal } from '@/hooks/useStoreModal'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import Modal from '@/components/ui/modal'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const formSchema = z.object({
+  name: z.string().min(1),
+})
 
 export const StoreModal = () => {
   const { isOpen, onClose } = useStoreModal()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  })
+
+  const onSubmitModal = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
 
   return (
     <Modal
@@ -11,7 +40,31 @@ export const StoreModal = () => {
       isOpen={isOpen}
       onClose={onClose}
     >
-      Criar sua loja virtual
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmitModal)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="p.ex: ecommerce" {...field} />
+                  </FormControl>
+                  <FormMessage title="obrigatório" />
+                </FormItem>
+              )}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+              <Button variant="outline" onClick={onClose}>
+                Cancelar
+              </Button>
+              <Button type="submit">Continuar</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   )
 }
