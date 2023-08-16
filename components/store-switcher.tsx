e4f0452eb81a7ad08ab4b2794edf92ca3e6eaea7
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-16 17:19:53 -0300

    create popover for store switcher

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,6 +1,32 @@
+'use client'
+
+import { useState } from 'react'
+import {
+  Check,
+  ChevronsUpDown,
+  PlusCircle,
+  Store as StoreIcon,
+} from 'lucide-react'
+import { useParams, useRouter } from 'next/navigation'
 import { Store } from '@prisma/client'
 
-import { PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import { useStoreModal } from '@/hooks/useStoreModal'
+import { Button } from '@/components/ui/button'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command'
 
 type PopoverTriggerProps = React.ComponentPropsWithRef<typeof PopoverTrigger>
 
@@ -8,6 +34,88 @@ interface StoreSwitcherProps extends PopoverTriggerProps {
   items: Store[]
 }
 
-export default function StoreSwitcher() {
-  return <div>store-switcher</div>
+export default function StoreSwitcher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
+  const storeModal = useStoreModal()
+  const params = useParams()
+  const router = useRouter()
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }))
+
+  const currentStore = formattedItems.find(
+    (item) => item.value === params.sotoreId
+  )
+
+  const [open, setOpen] = useState(false)
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setOpen(false)
+    router.push(`/${store.value}`)
+  }
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-expanded={open}
+          aria-label="Selecione uma loja"
+          className={cn('w-[200px] justify-between', className)}
+        >
+          <StoreIcon className="mr-2 w-4 h-4" />
+          {currentStore?.label}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Procure uma loja..." />
+            <CommandEmpty>Nenhuma loja encontrada</CommandEmpty>
+            <CommandGroup heading="Lojas">
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      'ml-auto h-4 w-4',
+                      currentStore?.value === store.value
+                        ? 'opac0'
+                        : 'opacity-0'
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setOpen(false)
+                  storeModal.onOpen()
+                }}
+              >
+                <PlusCircle className="mr-2 h-5 w-5" />
+                Criar nova loja
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
 }
