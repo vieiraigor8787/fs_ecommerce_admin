787edbe7ebc98082590c0842ebae37a18ccd806e
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-16 16:28:19 -0300

    settings route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
+import { MainNav } from './main-nav'
 
 export default function Navbar() {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <div>aqui será a troca de lojas</div>
-        <div>aqui serão os links</div>
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
