131bc38627d034020d316254af257a1cc3bff8fb
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-16 18:11:04 -0300

    settings form component

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Store } from '@prisma/client'
+
+interface SettingsFormProps {
+  initialData: Store
+}
+
+export const SettingsForm = ({ initialData }: SettingsFormProps) => {
+  return <div className="flex items-center justify-between"></div>
+}
--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation'
 import { auth } from '@clerk/nextjs'
 
 import prismadb from '@/lib/prismadb'
+import { SettingsForm } from './components/settings-form'
 
 interface SettingsProps {
   params: {
@@ -27,7 +28,13 @@ const SettingsPage = async ({ params }: SettingsProps) => {
     redirect('/')
   }
 
-  return <div className="">hello settings</div>
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  )
 }
 
 export default SettingsPage
