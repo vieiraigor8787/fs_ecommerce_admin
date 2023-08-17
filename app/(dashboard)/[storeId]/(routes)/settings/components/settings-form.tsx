e5c0eddaec7c1bb22a28464254cb32a3bdc3c6e0
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-17 11:21:04 -0300

    heading component for form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,11 +1,27 @@
 'use client'
 
+import { Trash } from 'lucide-react'
 import { Store } from '@prisma/client'
+
+import { Button } from '@/components/ui/button'
+import Heading from '@/components/ui/heading'
 
 interface SettingsFormProps {
   initialData: Store
 }
 
 export const SettingsForm = ({ initialData }: SettingsFormProps) => {
-  return <div className="flex items-center justify-between"></div>
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Configurações"
+          description="Configure suas preferências de loja"
+        />
+        <Button variant="destructive" size="icon" onClick={() => {}}>
+          <Trash className="w-4 h-4" />
+        </Button>
+      </div>
+    </>
+  )
 }
