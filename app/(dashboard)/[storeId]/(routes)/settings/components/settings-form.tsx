75600f9d6ac5698877d2c42bd8efd1abcecf9c75
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-17 12:24:14 -0300

    delete an update store

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -22,6 +22,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { AlertModal } from '@/components/modals/alert-modal'
 
 interface SettingsFormProps {
   initialData: Store
@@ -59,8 +60,31 @@ export const SettingsForm = ({ initialData }: SettingsFormProps) => {
     }
   }
 
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/stores/${params.storeId}`)
+      router.refresh()
+      router.push('/')
+      toast.success('Loja excluída')
+    } catch (error) {
+      toast.error(
+        'Tenha certeza que você excluiu todos os produtos e categorias primeiro.'
+      )
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading
           title="Configurações"
